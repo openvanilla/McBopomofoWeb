fb8e98100ad31126f787e2b22d96b3ab23d085d2
zonble <zonble@gmail.com>
2022-05-13 15:42:25 +0800

    Adds webpack settings.

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, 
+        include: [path.resolve(__dirname, "src")], 
+        use: "ts-loader", 
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  devtool: "eval-source-map", 
+  output: {
+    publicPath: "public",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+};
