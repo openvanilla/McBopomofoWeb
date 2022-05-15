7c7055b68d511cd7b3bf6172abd73f1c4e87fe5c
zonble <zonble@gmail.com>
2022-05-16 00:52:40 +0800

    Fixes the popup page of the Chrome extension.

---
+++ chomre-extension/content.js
@@ -0,0 +1,2 @@
+let inputArea = document.createElement("div");
+inputArea.innerHTML = '<div id="mcbpmf"><div name=\"mcbpmf_composing_buffer\" id=\"mcbpmf_composing_buffer\"></div><div name=\"mcbpmf_candidates\" id=\"mcbpmf_candidates\"></div></div>'
---
+++ chomre-extension/popup.js
@@ -0,0 +1,119 @@
+window.onload = () => {
+  let settings = (function () {
+    let that = {};
+    that.layout = "standard";
+    that.candidate_keys = "123456789";
+    that.select_phrase = "before_cursor";
+    that.esc_key_clear_entire_buffer = false;
+    that.move_cursor = false;
+    that.letter_mode = "upper";
+    return that;
+  })();
+
+  function applySettings(settings) {
+    {
+      let select = document.getElementById("layout");
+      let options = select.getElementsByTagName("option");
+      for (let option of options) {
+        if (option.value === settings.layout) {
+          debug(option.value);
+          option.selected = "selected";
+          break;
+        }
+      }
+    }
+    {
+      let select = document.getElementById("keys");
+      let options = select.getElementsByTagName("option");
+      let found = false;
+      for (let option of options) {
+        if (option.value === settings.candidate_keys) {
+          option.selected = "selected";
+          found = true;
+          break;
+        }
+      }
+    }
+    {
+      if (settings.select_phrase === "before_cursor") {
+        document.getElementById("before_cursor").checked = true;
+        document.getElementById("after_cursor").checked = false;
+      } else if (settings.select_phrase === "after_cursor") {
+        document.getElementById("before_cursor").checked = false;
+        document.getElementById("after_cursor").checked = true;
+      }
+    }
+    {
+      document.getElementById("esc_key").checked =
+        settings.esc_key_clear_entire_buffer;
+    }
+    {
+      document.getElementById("move_cursor").checked = settings.move_cursor;
+    }
+
+    {
+      if (settings.letter_mode === "upper") {
+        document.getElementById("uppercase_letters").checked = true;
+        document.getElementById("lowercase_letters").checked = false;
+      } else if (settings.letter_mode === "lower") {
+        document.getElementById("uppercase_letters").checked = false;
+        document.getElementById("lowercase_letters").checked = true;
+      }
+    }
+  }
+
+  function saveSettings(settings) {
+    chrome.storage.sync.set({ settings: settings }, () => {
+      debug(JSON.stringify(settings));
+    });
+  }
+
+  chrome.storage.sync.get("settings", (value) => {
+    settings = value.settings;
+    applySettings(value.settings);
+  });
+
+  document.getElementById("layout").onchange = (event) => {
+    let value = document.getElementById("layout").value;
+    settings.layout = value;
+    saveSettings(settings);
+  };
+
+  document.getElementById("keys").onchange = (event) => {
+    let value = document.getElementById("keys").value;
+    settings.candidate_keys = value;
+    saveSettings(settings);
+  };
+
+  document.getElementById("before_cursor").onchange = (event) => {
+    settings.select_phrase = "before_cursor";
+    saveSettings(settings);
+  };
+
+  document.getElementById("after_cursor").onchange = (event) => {
+    settings.select_phrase = "after_cursor";
+    saveSettings(settings);
+  };
+
+  document.getElementById("esc_key").onchange = (event) => {
+    let checked = document.getElementById("esc_key").checked;
+    settings.esc_key_clear_entire_buffer = checked;
+    saveSettings(settings);
+  };
+
+  document.getElementById("uppercase_letters").onchange = (event) => {
+    settings.letter_mode = "upper";
+    saveSettings(settings);
+  };
+
+  document.getElementById("lowercase_letters").onchange = (event) => {
+    settings.letter_mode = "lower";
+    saveSettings(settings);
+  };
+
+  document.getElementById("move_cursor").onchange = (event) => {
+    let checked = document.getElementById("move_cursor").checked;
+    settings.move_cursor = checked;
+    saveSettings(settings);
+  };
+};
