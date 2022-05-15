edbce3fc49b7bc7c55f51027f317aa8f135d6e98
zonble <zonble@gmail.com>
2022-05-16 02:04:14 +0800

    Updates the extension.

--- chomre-extension/content.js
+++ chomre-extension/content.js
@@ -1,4 +1,17 @@
 let inputArea = document.createElement("div");
 inputArea.innerHTML =
-  '<div id="mcbpmf"><div name="mcbpmf_composing_buffer" id="mcbpmf_composing_buffer"></div><div name="mcbpmf_candidates" id="mcbpmf_candidates"></div></div>';
+  '<div id="mcbpmf">' +
+  '<div name="mcbpmf_composing_buffer" id="mcbpmf_composing_buffer"></div>' +
+  '<div name="mcbpmf_candidates" id="mcbpmf_candidates"></div>' +
+  '<div name="mcbpmf_debug" id="mcbpmf_debug"></div>' +
+  "</div>";
 document.body.appendChild(inputArea);
+
+document.addEventListener("focusin", function (e) {
+  console.log("focusin!");
+  console.log(e);
+  document.getElementById("mcbpmf_composing_buffer").innerText =
+    e.target.nodeName.toString();
+  document.getElementById("mcbpmf_candidates").innerText =
+    e.srcElement.nodeName.toString();
+});
