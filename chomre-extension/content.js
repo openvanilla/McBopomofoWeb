8a1578ce4843438b812124fb77f2bc23417070ba
zonble <zonble@gmail.com>
2022-05-16 14:55:10 +0800

    Updates the chrome extension.

--- chomre-extension/content.js
+++ chomre-extension/content.js
@@ -8,39 +8,124 @@ inputArea.innerHTML =
 document.body.appendChild(inputArea);
 document.getElementById("mcbpmf").hidden = true;
 
-window.currentElement = null;
-window.myKeyUp = function (event) {
+window.createMcBopomofoUI = function (element) {
+  let that = {};
+
+  that.chineseMode = true;
+  that.element = element;
+
+  that.inputPanel = function () {
+    return document.getElementById("mcbpmf");
+  };
+
+  that.composingBuffer = function () {
+    return document.getElementById("mcbpmf_composing_buffer");
+  };
+
+  that.candidatesPanel = function () {
+    return document.getElementById("mcbpmf_candidates");
+  };
+
+  that.commitString = function (string) {
+    if (
+      that.element.nodeName === "TEXTAREA" ||
+      that.element.nodeName === "INPUT"
+    ) {
+      var selectionStart = that.element.selectionStart;
+      var selectionStop = that.element.selectionStop;
+      var text = document.that.element.value;
+      var head = text.substring(0, selectionStart);
+      var tail = text.substring(selectionStop);
+      that.element.value = head + string + tail;
+      let start = selectionStart + string.length;
+      that.element.setSelectionRange(start, start);
+    }
+  };
+
+  that.update = function (string) {
+    let state = JSON.parse(string);
+    {
+      let buffer = state.composingBuffer;
+      let s = that.chineseMode ? "【麥】" : "【英】";
+      let i = 0;
+      for (let item of buffer) {
+        let text = item.text;
+        for (let c of text) {
+          if (i === state.cursorIndex) {
+            s += "<span class='cursor'>|</span>";
+          }
+          s += c;
+          i++;
+        }
+      }
+      if (i === state.cursorIndex) {
+        s += "<span class='cursor'>|</span>";
+      }
+      composingBuffer().innerHTML = s;
+    }
+
+    if (state.candidates.length) {
+      let s = "<table><tr>";
+      for (let candidate of state.candidates) {
+        s += "<td>";
+        if (candidate.selected) s += "<b>";
+        s += candidate.keyCap;
+        if (candidate.selected) s += "</b>";
+        s += "</td>";
+        s += "<td>";
+        if (candidate.selected) s += "<b>";
+        s += candidate.candidate;
+        if (candidate.selected) s += "</b>";
+        s += "</td>";
+        if (candidate.selected) s += "</b>";
+      }
+      s += "</tr></table>";
+      candidatesPanel().innerHTML = s;
+    }
+  };
+
+  return that;
+};
+
+window.mcbpmf_KeyUp = function (event) {
   console.log("keyup");
   console.log(event);
 };
 
-window.myKeydown = function (event) {
+window.mcbpmf_Keydown = async function (event) {
   console.log("keydown");
-  console.log(event);
-  let ce = window.currentElement;
-  if (ce.nodeName == "DIV") {
-    console.log("selection");
-    var sel = window.getSelection();
-    console.log(sel);
-    document.getElementById("mcbpmf_candidates").innerText =
-      sel.focusOffset + "-" + (sel.focusOffset + sel.rangeCount - 1);
-    // let s = window.currentElement.innerText;
-    // window.currentElement.innerText = s + "a";
-  } else {
-    document.getElementById("mcbpmf_candidates").innerText =
-      ce.selectionStart + "-" + ce.selectionEnd;
-    // let s = window.currentElement.value;
-    // window.currentElement.value = s + "a";
-  }
+  console.log("keydown 1");
+  let promise = new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(
+      { command: "send_key_event", key: event },
+      function (response) {
+        console.log("response comes");
+        console.log(response);
+        console.log("keydown 2");
+        event.preventDefault();
+        resolve();
+      }
+    );
+  });
+  await promise;
+  console.log("keydown 3");
 };
 
+document.addEventListener("focusout", function (e) {
+  console.log("focusout");
+  document.getElementById("mcbpmf").hidden = true;
+});
+
 document.addEventListener("focusin", function (e) {
-  if (window.currentElement != undefined) {
-    window.currentElement.removeEventListener("keyup", myKeyUp);
-    window.currentElement.removeEventListener("keydown", myKeydown);
+  if (window.mcBopomofoUI != undefined) {
+    let element = window.mcBopomofoUI.element;
+    element.removeEventListener("keyup", mcbpmf_KeyUp);
+    element.removeEventListener("keydown", mcbpmf_Keydown);
   }
+
   console.log(e.target);
   let nodeName = e.target.nodeName;
+
   let newElement;
   if (nodeName === "TEXTAREA") {
     newElement = e.target;
@@ -53,15 +138,28 @@ document.addEventListener("focusin", function (e) {
   }
 
   if (newElement == undefined) {
-    window.currentElement = null;
+    window.mcBopomofoUI = null;
     document.getElementById("mcbpmf").hidden = true;
     return;
   }
 
-  document.getElementById("mcbpmf").hidden = false;
-  window.currentElement = newElement;
-  window.currentElement.addEventListener("keyup", myKeyUp);
-  window.currentElement.addEventListener("keydown", myKeydown);
+  window.mcBopomofoUI = createMcBopomofoUI(newElement);
+  window.mcBopomofoUI.inputPanel().hidden = false;
+  newElement.addEventListener("keyup", mcbpmf_KeyUp);
+  newElement.addEventListener("keydown", mcbpmf_Keydown);
+
+  console.log("call set_ui " + window.mcBopomofoUI);
+  chrome.runtime.sendMessage(
+    { command: "set_ui", ui: window.mcBopomofoUI },
+    function (response) {
+      console.log(response);
+    }
+  );
+
+  console.log("call reset");
+  chrome.runtime.sendMessage({ command: "reset" }, function (response) {
+    console.log(response);
+  });
 
   document.getElementById("mcbpmf_candidates").innerText =
     e.srcElement.nodeName.toString();
