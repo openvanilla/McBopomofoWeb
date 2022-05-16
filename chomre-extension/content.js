5f0384c9c2f14735664a1ddd4026ee4d6441e6aa
zonble <zonble@gmail.com>
2022-05-16 10:08:27 +0800

    Updates the extension.

--- chomre-extension/content.js
+++ chomre-extension/content.js
@@ -6,12 +6,63 @@ inputArea.innerHTML =
   '<div name="mcbpmf_debug" id="mcbpmf_debug"></div>' +
   "</div>";
 document.body.appendChild(inputArea);
+document.getElementById("mcbpmf").hidden = true;
+
+window.currentElement = null;
+window.myKeyUp = function (event) {
+  console.log("keyup");
+  console.log(event);
+};
+
+window.myKeydown = function (event) {
+  console.log("keydown");
+  console.log(event);
+  let ce = window.currentElement;
+  if (ce.nodeName == "DIV") {
+    console.log("selection");
+    var sel = window.getSelection();
+    console.log(sel);
+    document.getElementById("mcbpmf_candidates").innerText =
+      sel.focusOffset + "-" + (sel.focusOffset + sel.rangeCount - 1);
+    // let s = window.currentElement.innerText;
+    // window.currentElement.innerText = s + "a";
+  } else {
+    document.getElementById("mcbpmf_candidates").innerText =
+      ce.selectionStart + "-" + ce.selectionEnd;
+    // let s = window.currentElement.value;
+    // window.currentElement.value = s + "a";
+  }
+};
 
 document.addEventListener("focusin", function (e) {
-  console.log("focusin!");
-  console.log(e);
-  document.getElementById("mcbpmf_composing_buffer").innerText =
-    e.target.nodeName.toString();
+  if (window.currentElement != undefined) {
+    window.currentElement.removeEventListener("keyup", myKeyUp);
+    window.currentElement.removeEventListener("keydown", myKeydown);
+  }
+  console.log(e.target);
+  let nodeName = e.target.nodeName;
+  let newElement;
+  if (nodeName === "TEXTAREA") {
+    newElement = e.target;
+  } else if (nodeName === "DIV") {
+    newElement = e.target;
+  } else if (nodeName === "INPUT") {
+    if (e.target.type === "text") {
+      newElement = e.target;
+    }
+  }
+
+  if (newElement == undefined) {
+    window.currentElement = null;
+    document.getElementById("mcbpmf").hidden = true;
+    return;
+  }
+
+  document.getElementById("mcbpmf").hidden = false;
+  window.currentElement = newElement;
+  window.currentElement.addEventListener("keyup", myKeyUp);
+  window.currentElement.addEventListener("keydown", myKeydown);
+
   document.getElementById("mcbpmf_candidates").innerText =
     e.srcElement.nodeName.toString();
 });
