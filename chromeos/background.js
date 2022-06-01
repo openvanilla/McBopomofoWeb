f0ff924101d3d7493b5291c0b71796d216c1a429
zonble <zonble@gmail.com>
2022-06-01 22:55:44 +0800

    Makes the extension to work.

--- chromeos/background.js
+++ chromeos/background.js
@@ -9,7 +9,7 @@ window.onload = function () {
     that.engineID = engineID;
 
     that.reset = function () {
-      chrome.input.ime.clearComposition({ contextID: mcContext });
+      chrome.input.ime.clearComposition({ contextID: mcContext.contextID });
       chrome.input.ime.setCandidateWindowProperties({
         engineID: that.engineID,
         properties: {
@@ -20,7 +20,7 @@ window.onload = function () {
 
     that.commitString = function (string) {
       chrome.input.ime.commitText({
-        contextID: mcContext,
+        contextID: mcContext.contextID,
         text: string,
       });
     };
@@ -58,7 +58,7 @@ window.onload = function () {
       }
 
       chrome.input.ime.setComposition({
-        contextID: this.context,
+        contextID: mcContext.contextID,
         cursor: state.cursorIndex,
         segments: segments,
         text: text,
@@ -67,34 +67,36 @@ window.onload = function () {
       });
 
       if (candidates.length) {
-        chrome.input.ime.setCandidateWindowProperties({
-          engineID: that.engineID,
-          properties: {
-            visible: true,
-            cursorVisible: false,
-            vertical: false,
-            pageSize: candidates.length,
-          },
-        });
         let chromeCandidates = [];
         let index = 0;
         let selectedIndex = 0;
         for (let candidate of state.candidates) {
-          var item = {};
           if (candidate.selected) {
             selectedIndex = index;
           }
+          var item = {};
           item.candidate = candidate.candidate;
+          item.annotation = "";
           item.id = index++;
           item.label = candidate.keyCap;
           chromeCandidates.push(item);
         }
+        console.log(chromeCandidates);
+        chrome.input.ime.setCandidateWindowProperties({
+          engineID: that.engineID,
+          properties: {
+            visible: true,
+            cursorVisible: true,
+            vertical: true,
+            pageSize: candidates.length,
+          },
+        });
         chrome.input.ime.setCandidates({
-          contextID: this.context,
-          candidates: candidates,
+          contextID: mcContext.contextID,
+          candidates: chromeCandidates,
         });
         chrome.input.ime.setCursorPosition({
-          contextID: this.context,
+          contextID: mcContext.contextID,
           candidateID: selectedIndex,
         });
       } else {
@@ -110,7 +112,7 @@ window.onload = function () {
   }
 
   chrome.input.ime.onActivate.addListener(function (engineID) {
-    mcInputController = new InputController(ui);
+    mcInputController = new InputController(makeUI(engineID));
     mcEngineID = engineID;
     var menus = [
       { id: "mcbopomofo-options", label: "Options", style: "check" },
@@ -128,7 +130,7 @@ window.onload = function () {
       return false;
     }
 
-    return controller.keyEvent(keyData);
+    return mcInputController.keyEvent(keyData);
   });
 
   chrome.input.ime.onMenuItemActivated.addListener(function (engineID, name) {
