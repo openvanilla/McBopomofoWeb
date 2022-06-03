c1c7d726007a596f5994f15159f72393895bddc4
zonble <zonble@gmail.com>
2022-06-03 21:02:49 +0800

    Adds icons.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+/**
+ * @license
+ * Copyright (c) 2022 and onwards The McBopomofo Authors.
+ * This code is released under the MIT license.
+ * SPDX-License-Identifier: MIT
+ */
+
 export { InputController } from "./McBopomofo/InputController";
