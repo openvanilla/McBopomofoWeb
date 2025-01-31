aa1d8447ff56383689c423fa920eee0c8e09fc70
zonble <zonble@gmail.com>
2025-02-01 02:38:41 +0800

    Adds tests.

--- src/McBopomofo/KeyHandler.test.ts
+++ src/McBopomofo/KeyHandler.test.ts
@@ -8,8 +8,17 @@
 import { KeyHandler } from "./KeyHandler";
 import { WebLanguageModel } from "./WebLanguageModel";
 import { webData } from "./WebData";
-import { Empty, InputState, Inputting } from "./InputState";
-import { Key } from "./Key";
+import {
+  ChoosingCandidate,
+  Committing,
+  Empty,
+  EmptyIgnoringPrevious,
+  InputState,
+  Inputting,
+} from "./InputState";
+import { Key, KeyName } from "./Key";
+import exp from "constants";
+import { BopomofoKeyboardLayout } from "../Mandarin";
 
 function asciiKey(input: string[]): Key[] {
   let keys: Key[] = [];
@@ -44,6 +53,80 @@ describe("Test KeyHandler.test", () => {
 
   afterEach(() => {});
 
+  test("Test languageCode", () => {
+    expect(keyHandler.languageCode).toBe("");
+    keyHandler.languageCode = "zh-TW";
+    expect(keyHandler.languageCode).toBe("zh-TW");
+    keyHandler.languageCode = "en";
+    expect(keyHandler.languageCode).toBe("en");
+  });
+
+  test("Test selectPhraseAfterCursorAsCandidate", () => {
+    keyHandler.selectPhraseAfterCursorAsCandidate = true;
+    expect(keyHandler.selectPhraseAfterCursorAsCandidate).toBe(true);
+    let keys = asciiKey(["s", "u", "3", "c", "l", "3"]);
+    let leftKey = Key.namedKey(KeyName.LEFT);
+    let spaceKey = Key.namedKey(KeyName.SPACE);
+    keys.push(leftKey);
+    keys.push(spaceKey);
+    let state = handleKeySequence(keyHandler, keys);
+    expect(state instanceof ChoosingCandidate).toBe(true);
+    let choosingCandidate = state as ChoosingCandidate;
+    expect(choosingCandidate.candidates[2].value).toBe("好");
+  });
+
+  test("Test selectPhraseAfterCursorAsCandidate", () => {
+    keyHandler.selectPhraseAfterCursorAsCandidate = false;
+    expect(keyHandler.selectPhraseAfterCursorAsCandidate).toBe(false);
+    let keys = asciiKey(["s", "u", "3", "c", "l", "3"]);
+    let leftKey = Key.namedKey(KeyName.LEFT);
+    let spaceKey = Key.namedKey(KeyName.SPACE);
+    keys.push(leftKey);
+    keys.push(spaceKey);
+    let state = handleKeySequence(keyHandler, keys);
+    expect(state instanceof ChoosingCandidate).toBe(true);
+    let choosingCandidate = state as ChoosingCandidate;
+    expect(choosingCandidate.candidates[2].value).toBe("你");
+  });
+
+  test("Test escKeyClearsEntireComposingBuffer", () => {
+    keyHandler.escKeyClearsEntireComposingBuffer = true;
+    expect(keyHandler.escKeyClearsEntireComposingBuffer).toBe(true);
+    let keys = asciiKey(["s", "u", "3", "c", "l", "3"]);
+    let esc = Key.namedKey(KeyName.ESC);
+    keys.push(esc);
+    let state = handleKeySequence(keyHandler, keys);
+    console.log(state);
+    expect(state instanceof EmptyIgnoringPrevious).toBe(true);
+  });
+
+  test("Test keyboardLayout", () => {
+    expect(keyHandler.keyboardLayout).toBe(
+      BopomofoKeyboardLayout.StandardLayout
+    ); // StandardLayout is 0
+    keyHandler.keyboardLayout = BopomofoKeyboardLayout.ETenLayout; // ETenLayout is 1
+    expect(keyHandler.keyboardLayout).toBe(BopomofoKeyboardLayout.ETenLayout);
+
+    // Test typing with ETen layout
+    let keys = asciiKey(["n", "e", "3"]); // ETen layout keys for "你"
+    let state = handleKeySequence(keyHandler, keys);
+    expect(state instanceof Inputting).toBe(true);
+    let inputting = state as Inputting;
+    expect(inputting.composingBuffer).toBe("你");
+    expect(inputting.cursorIndex).toBe(1);
+
+    keyHandler.keyboardLayout = BopomofoKeyboardLayout.HsuLayout; // HsuLayout is 2
+    expect(keyHandler.keyboardLayout).toBe(BopomofoKeyboardLayout.HsuLayout);
+
+    // Test typing with Hsu layout
+    keys = asciiKey(["n", "e", "f"]); // Hsu layout keys for "你"
+    state = handleKeySequence(keyHandler, keys);
+    expect(state instanceof Inputting).toBe(true);
+    inputting = state as Inputting;
+    expect(inputting.composingBuffer).toBe("你你");
+    expect(inputting.cursorIndex).toBe(2);
+  });
+
   test("Test empty key", () => {
     let stateCallbackCalled = false;
     let errorCallbackCalled = false;
@@ -73,5 +156,51 @@ describe("Test KeyHandler.test", () => {
     let inputting = state as Inputting;
     expect(inputting.composingBuffer).toBe("你好");
     expect(inputting.cursorIndex).toBe(2);
+  });
+
+  test("Test commit", () => {
+    let keys = asciiKey(["s", "u", "3", "c", "l", "3"]);
+    let enter = Key.namedKey(KeyName.RETURN);
+    keys.push(enter);
+    let state = handleKeySequence(keyHandler, keys);
+    expect(state instanceof Committing).toBe(true);
+    let committing = state as Committing;
+    expect(committing.text).toBe("你好");
+  });
+
+  test("Test Punctuation", () => {
+    let keys = asciiKey(["s", "u", "3", "c", "l", "3"]);
+    let comma = new Key("<", KeyName.UNKNOWN, true, false, false);
+    keys.push(comma);
+    let state = handleKeySequence(keyHandler, keys);
+    expect(state instanceof Inputting).toBe(true);
+    let inputting = state as Inputting;
+    expect(inputting.composingBuffer).toBe("你好，");
+  });
+
+  test("Test Tab 1", () => {
+    let keys = asciiKey(["s", "u", "3", "c", "l", "3"]);
+    let enter = Key.namedKey(KeyName.TAB);
+    keys.push(enter);
+    let state = handleKeySequence(keyHandler, keys);
+    expect(state instanceof Inputting).toBe(true);
+    let inputting = state as Inputting;
+    expect(inputting.composingBuffer).toBe("妳好");
+  });
+
+  test("Test Tab 2", () => {
+    let keys = [];
+    let tab = Key.namedKey(KeyName.TAB);
+    keys.push(tab);
+    let state = handleKeySequence(keyHandler, keys);
+    expect(state instanceof Empty).toBe(true);
+  });
+
+  test("Test Tab 2", () => {
+    let keys = asciiKey(["s", "u"]);
+    let tab = Key.namedKey(KeyName.TAB);
+    keys.push(tab);
+    let state = handleKeySequence(keyHandler, keys);
+    expect(state instanceof Inputting).toBe(true);
   });
 });
