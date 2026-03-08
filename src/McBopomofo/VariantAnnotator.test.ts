c4fb91c1572bea995d6219cf47a82ec6b23e5169
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-03-08 22:21:54 +0800

    test: Add new test for VariantAnnotator's annotate method and refactor text rendering in example output.

--- src/McBopomofo/VariantAnnotator.test.ts
+++ src/McBopomofo/VariantAnnotator.test.ts
@@ -3,17 +3,33 @@ import { webBpmfvsPua } from "./WebBpmfvsPua";
 import { webBpmfvsVariants } from "./WebBpmfvsVariants";
 
 describe("VariantAnnotator", () => {
-  it("should not havw variant for 還", () => {
+  it("should not havw variant for 還 1", () => {
     const va = new VariantAnnotator(webBpmfvsPua, webBpmfvsVariants);
     let result = va.annotateSingleCharacter("還", "ㄏㄞˊ");
     expect(result.hasVariantSelectors).toBe(false);
     expect(result.annotatedString.length === 1);
   });
 
-  it("should have variant for  1", () => {
+  it("should have variant for 還 2", () => {
     const va = new VariantAnnotator(webBpmfvsPua, webBpmfvsVariants);
     let result = va.annotateSingleCharacter("還", "ㄏㄨㄢˊ");
     expect(result.hasVariantSelectors).toBe(true);
     expect(result.annotatedString.length === 3);
+  });
+
+  it("should workd with annotate", () => {
+    const va = new VariantAnnotator(webBpmfvsPua, webBpmfvsVariants);
+    let result = va.annotate(["還", "還"], ["ㄏㄨㄢˊ", "ㄏㄨㄢˊ"]);
+    expect(result.hasVariantSelectors).toBe(true);
+    expect(result.annotatedString.length === 6);
+
+    var s = "";
+    for (let i = 0; i < 6; i++) {
+      if (i === 3) {
+        s += " | ";
+      }
+      s += result.annotatedString[i];
+    }
+    expect(s === "還󠇡 | 還󠇡");
   });
 });
