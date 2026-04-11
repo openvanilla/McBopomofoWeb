aa171be3eb201f26cec79cc3c6216fa0ab0359d8
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-04-11 23:54:58 +0800

    chore: format jest.config.js and add newlines in mcp.ts for consistency

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   collectCoverage: true,
   reporters: ["default", "jest-junit"],
-  coverageReporters: ["json", "html",  "clover", "json-summary"],
+  coverageReporters: ["json", "html", "clover", "json-summary"],
+  coveragePathIgnorePatterns: ["mcp.ts", "pime.ts", "chromeos_ime.ts"],
 };
