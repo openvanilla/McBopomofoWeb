6d37e62696a6111a61cbbe03277e1fec0935e022
Weizhong Yang a.k.a zonble <zonble@gmail.com>
2026-04-19 12:07:32 +0800

    feat: add options pages for ChromeOS and PIME and update PIME build script

--- build_pime.bat
+++ build_pime.bat
@@ -9,17 +9,12 @@ if %errorLevel% neq 0 (
     exit /b 1
 )
 
-set IME_NAME=mcbopomofo
-
-echo * Build %IME_NAME% for PIME
+echo * Build McBopomofo for PIME
 call npm run build:pime
 echo * Delete old files
-rmdir /S /Q "C:\Program Files (x86)\PIME\node\input_methods\%IME_NAME%"
+rmdir /S /Q "C:\Program Files (x86)\PIME\node\input_methods\mcbopomofo"
 echo * Copy new files
-xcopy /E /I ".\output\pime" "C:\Program Files (x86)\PIME\node\input_methods\%IME_NAME%"
-
-@REM echo * Register %IME_NAME%
-@REM "C:\Program Files (x86)\PIME\node\input_methods\%IME_NAME%\run_register_ime.bat"
+xcopy /E /I ".\output\pime" "C:\Program Files (x86)\PIME\node\input_methods\mcbopomofo"
 
 
 echo "Please restart PIME Launcher to see the changes."
